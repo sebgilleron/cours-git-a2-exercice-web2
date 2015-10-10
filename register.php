cf0918c5a66fb75f12f5e603d42592b7e1f53ee2
Sébastien Gilleron <seb.gilleron@hotmail.fr>
2015-10-10 12:13:50 +0200

    Finalisation de l'inscription. Fix #2

--- register.php
+++ register.php
@@ -5,9 +5,14 @@ require('model/functions.fn.php');
 /********************************
 			PROCESS
 ********************************/
+ 
 
-if(isset($_POST) && !empty($_POST)) {
+if(isset($_POST['email']) && !empty($_POST['email']) && isset ($_POST['username']) && !empty ($_POST['username']) && isset ($_POST['password']) && !empty ($_POST['password'])) {
 
+    
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password';
 	/* isEmailAvailable
 		return :
 			true if available
@@ -15,7 +20,9 @@ if(isset($_POST) && !empty($_POST)) {
 		$db -> 				database object
 		$email -> 			field value : email
 	*/
-	$email_ok = isEmailAvailable($db, "git@initiation.com");
+       
+    
+	$email_ok = isEmailAvailable($db,$email);
 
 	/* isUsernameAvailable
 		return :
@@ -24,7 +31,10 @@ if(isset($_POST) && !empty($_POST)) {
 		$db -> 				database object
 		$username -> 			field value : username
 	*/
-	$username_ok = isUsernameAvailable($db, "Git");
+          
+    
+    
+	$username_ok = isUsernameAvailable($db,$username);
 
 
 	if ($email_ok && $username_ok) {
@@ -37,16 +47,22 @@ if(isset($_POST) && !empty($_POST)) {
 			$email -> 			field value : email
 			$password -> 		field value : password
 		*/
-		userRegistration($db, "Git", "git@initiation.com", "password");
+        
+        $db = db;
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password';
+        
+		userRegistration($db, $username, $email, $password);
 		header('Location: login.php');
 	}
 
 	if (!$email_ok) {
-		//
+		$error = "Email déjà pris";
 	}
 
 	if (!$username_ok) {
-		//
+		$error = "Nom d'utilisateur déjà pris";
 	}
 
 }
